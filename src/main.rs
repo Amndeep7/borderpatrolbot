b6291dc61d267a0aec164c122fa83129e85c41e5
Amndeep Singh Mann <amndeep.singh.mann@gmail.com>
2016-07-31 04:05:23 +0000

    Added trying to log in using token

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,20 @@
 extern crate discord;
 
+use std::fs::File;
+use std::io::Read;
+
 use discord::Discord;
 
-fn main() {
-    println!("Hello, world!");
+fn read_token_file(name: &str) -> String {
+    let mut token = String::new();
+    let mut f = File::open(&name).expect("Unable to open the token file");
+    f.read_to_string(&mut token).expect("Unable to read the token file");
+    token
+}
 
-    let discord = Discord::from_bot_token().expect("login failed");
+fn main() {
+    let token = read_token_file("token");
+    println!("{}", token);
+    let discord = Discord::from_bot_token(&token).expect("login failed");
     discord.logout().expect("logout failed");
 }
